dee1a86ef254c3c548d590e78c62af1cfde9ec9d
= <=>
2020-12-25 13:45:08 +0900

    1st commit

---
+++ sample01/src/main/java/jp/winschool/spring/sample01/Sample01Controller.java
@@ -0,0 +1,21 @@
+package jp.winschool.spring.sample01;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Sample01Controller {
+    @GetMapping("/")
+    public String index() {
+    	return "index";
+    }
+    
+    @PostMapping("/result")
+    public String result(@RequestParam String name, Model model) {
+    	model.addAttribute("name", name);
+    	return "result";
+    }
+}
